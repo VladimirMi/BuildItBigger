20cf4b453f62fec7b70f3d45189baef0ba293ab1
VladimirMikhalev <vladimirmsm@yandex.ru>
2018-03-28 21:58:30 +0300

    Added test task

--- app/src/free/java/com/udacity/gradle/builditbigger/FreeApp.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/FreeApp.java
@@ -1,12 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.Application;
+
 import com.google.android.gms.ads.MobileAds;
 
 /**
  * Created by Vladimir Mikhalev 25.03.2018.
  */
 
-public class FreeApp extends App {
+public class FreeApp extends Application {
 
     @Override
     public void onCreate() {
